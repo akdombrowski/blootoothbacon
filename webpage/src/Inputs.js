f16acc33ad781ee8923e1fdb6c5e1c33a580f812
akdombrowski <akdflyinvee73@gmail.com>
2019-10-20 14:15:58 -0500

    update text fields

--- webpage/src/Inputs.js
+++ webpage/src/Inputs.js
@@ -19,15 +19,39 @@ import { makeStyles } from "@material-ui/core/styles";
 export default class Inputs extends React.Component {
   constructor(props) {
     super(props);
+    this.handleSaltChange = this.handleSaltChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handlePasswordChange = event => {
+    console.log(event.target.value);
+    this.setState({
+      testPassword: event.target.value
+    });
+  };
+
+  handleSaltChange = event => {
+    console.log(event.target.value);
+    this.setState({
+      salt: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    console.log("submit");
+    console.log(event.target);
+    event.preventDefault();
+  };
+
   render() {
+    let statepassword;
     return (
       <Container maxWidth="xs">
         <Typography component="h1" variant="h5">
           Password Hashing Checker
         </Typography>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <TextField
             required
             fullWidth
@@ -36,6 +60,9 @@ export default class Inputs extends React.Component {
             placeholder="Salt"
             margin="normal"
             variant="outlined"
+            onChange={this.handleSaltChange}
+            value={this.state ? this.state.salt : ""}
+            InputLabelProps={{ shrink: true }}
           />
 
           <TextField
@@ -46,7 +73,9 @@ export default class Inputs extends React.Component {
             placeholder="Test Password"
             margin="normal"
             variant="outlined"
-            onChange={this.props.handleChange('name')}
+            onChange={this.handlePasswordChange}
+            value={this.state ? this.state.hashedPassword : ""}
+            InputLabelProps={{ shrink: true }}
           />
           <Button
             type="submit"
@@ -55,17 +84,19 @@ export default class Inputs extends React.Component {
             color="primary"
             onClick={this.props.hashing}
           >
-            Hash
+            Hash It
           </Button>
-
           <TextField
-            disabled
             fullWidth
-            id="standard-required"
+            id="outlined-read-only-input"
             label="Hashed Password Value"
-            placeholder="Hashed Password Value"
+            defaultValue="Hashed Password Value"
             margin="normal"
+            InputProps={{
+              readOnly: true
+            }}
             variant="outlined"
+            InputLabelProps={{ shrink: true }}
           />
         </form>
       </Container>
